674c21924a474666b11ec6822bdf01d44ddc5b68
Julio Pavón <julio@jpavon.com>
2017-11-07 12:27:26 +0800

    remove devtools for now

--- src/rootContainer.js
+++ src/rootContainer.js
@@ -8,21 +8,14 @@ import Workout from 'pages/Workout'
 import Lifts from 'pages/Lifts'
 import NoMatch from 'pages/NoMatch'
 
-// uncommented until https://github.com/gaearon/redux-devtools/issues/381
-// let DevTools
-// if (process.env.NODE_ENV !== 'production') DevTools = require('./DevTools').default;
-
 const Root = ({ store }) => (
     <Provider store={store}>
-        <div>
-            <Switch>
-                <Route exact path="/" component={Home} />
-                <Route exact path="/workouts/:id" component={Workout} />
-                <Route exact path="/lifts" component={Lifts} />
-                <Route component={NoMatch}/>
-            </Switch>
-            {/* process.env.NODE_ENV !== 'production' && <DevTools /> */}
-        </div>
+        <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/workouts/:id" component={Workout} />
+            <Route exact path="/lifts" component={Lifts} />
+            <Route component={NoMatch}/>
+        </Switch>
     </Provider>
 )
 
