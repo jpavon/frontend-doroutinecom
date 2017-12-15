e4c13ce299a5f6edf26c57e1b23ccb1ee9b97e13
Julio Pavón <julio@jpavon.com>
2017-12-15 21:40:31 +0100

    add gatsby for landing page

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -5,41 +5,42 @@ import logo from 'media/logo.svg'
 
 import './style.css'
 
-const Nav = ({isAuth}) => (
+const Nav = ({isAuth = false, renderItems = null}) => (
     <nav className="nav">
         <NavLink to="/" className="nav-logo" activeClassName="nav-link--active">
             <img src={logo} alt="Logo"/>
         </NavLink>
 
         <ul className="nav-list">
-            {isAuth &&
-                <Fragment>
-                    <li className="nav-item">
-                        <NavLink to="/routines" className="nav-link" activeClassName="nav-link--active">
-                            Routines
-                        </NavLink>
-                    </li>
-                    <li className="nav-item">
-                        <NavLink to="/settings" className="nav-link" activeClassName="nav-link--active">
-                            Settings
-                        </NavLink>
-                    </li>
-                </Fragment>
-            }
-
-            {!isAuth &&
-                <Fragment>
-                    <li className="nav-item">
-                        <NavLink to="/login" className="nav-link" activeClassName="nav-link--active">
-                            Login
-                        </NavLink>
-                    </li>
-                    <li className="nav-item">
-                        <NavLink to="/register" className="nav-link" activeClassName="nav-link--active">
-                            Register
-                        </NavLink>
-                    </li>
-                </Fragment>
+            {renderItems ||
+                (
+                    isAuth ?
+                        <Fragment>
+                            <li className="nav-item">
+                                <NavLink to="/routines" className="nav-link" activeClassName="nav-link--active">
+                                    Routines
+                                </NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink to="/settings" className="nav-link" activeClassName="nav-link--active">
+                                    Settings
+                                </NavLink>
+                            </li>
+                        </Fragment>
+                    :
+                        <Fragment>
+                            <li className="nav-item">
+                                <NavLink to="/login" className="nav-link" activeClassName="nav-link--active">
+                                    Login
+                                </NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink to="/register" className="nav-link" activeClassName="nav-link--active">
+                                    Register
+                                </NavLink>
+                            </li>
+                        </Fragment>
+                )
             }
         </ul>
     </nav>
