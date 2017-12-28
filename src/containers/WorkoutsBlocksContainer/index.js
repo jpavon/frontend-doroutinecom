2700974297082642f2e0db52a954b54d9a2b71ba
Julio Pavón <julio@jpavon.com>
2017-12-28 18:07:27 +0100

    cleaning

--- src/containers/WorkoutsBlocksContainer/index.js
+++ src/containers/WorkoutsBlocksContainer/index.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
-import union from 'lodash/union'
 
 import { blocksWorkoutsSelector } from 'data/workouts/selectors'
 
