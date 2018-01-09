90503c3f3defe98764015734c95a9a242e45e4aa
Julio Pavón <julio@jpavon.com>
2018-01-09 11:31:08 +0100

    remove transform scale from transiton, fixes z-index issues

--- src/components/Transition/index.js
+++ src/components/Transition/index.js
@@ -3,24 +3,22 @@ import { TransitionMotion, spring } from 'react-motion'
 
 const willEnter = () => ({
     opacity: 0,
-    scale: 0.98,
 })
 
 const willLeave = () => ({
     opacity: spring(0),
-    scale: spring(1.02),
 })
 
 const getStyles = () => ({
     opacity: spring(1),
-    scale: spring(1),
 })
 
 const Transition = ({children, className, noData}) => (
     <TransitionMotion
         styles={children.length > 0 ? children.map((item) => ({ key: `${item.key}`, style: getStyles(), data: item })) : []}
         willLeave={willLeave}
-        willEnter={willEnter}>
+        willEnter={willEnter}
+    >
         {(interpolatedStyles) =>
             <Fragment>
                 {interpolatedStyles.length > 0 ? interpolatedStyles.map(({ key, style, data }) =>
@@ -28,7 +26,6 @@ const Transition = ({children, className, noData}) => (
                         key={ `${key}-transition` }
                         style={{
                             opacity: style.opacity,
-                            transform: `scale(${style.scale})`
                         }}
                         className={className}
                     >
