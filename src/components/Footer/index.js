619a4e921672294c857289ff4eeb3b4bd4e2339f
Julio Pavón <julio@jpavon.com>
2018-01-12 20:15:17 +0100

    update logo

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,10 +5,8 @@ import './style.css'
 const Footer = () => (
     <div className="footer">
         <div className="container">
-            <a className="footer-item" href="http://localhost:3000">App</a>
-            <a className="footer-item" href="http://localhost:8001">Landing page</a>
             <a className="footer-item" href="http://localhost:8001">Support</a>
-            <span className="footer-item">Gym app 2018</span>
+            <span className="footer-item">Saveroutine 2018</span>
         </div>
     </div>
 )
