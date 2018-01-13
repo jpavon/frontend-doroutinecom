0f1adf4f740a579ef10a8f103c5de93df64e2f01
Julio Pavón <julio@jpavon.com>
2018-01-13 16:09:52 +0100

    minor changes

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,11 +1,16 @@
 import React from 'react'
 
+import env from 'env'
+
 import './style.css'
 
 const Footer = () => (
     <div className="footer">
         <div className="container">
+            <a className="footer-item" href={env.HOME_URL}>Home</a>
             <a className="footer-item" href="http://localhost:8001">Support</a>
+            <br />
+            <br />
             <span className="footer-item">Saveroutine 2018</span>
         </div>
     </div>
