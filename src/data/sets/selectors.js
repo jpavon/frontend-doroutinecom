1fe89ff5f814c669d7be72b5786b3cbd6606bcf4
Julio Pavón <julio@jpavon.com>
2018-02-01 16:08:45 +0100

    clean sets selector

--- src/data/sets/selectors.js
+++ src/data/sets/selectors.js
@@ -1,25 +1,17 @@
 import { createSelector } from 'reselect'
-// import isFinite from 'lodash/isFinite'
 
 import { liftExerciseSelector } from 'data/lifts/selectors'
 import Set from 'data/sets/schema'
 
-// const round = (x) => {
-//     const number = Math.round(x * 1) / 1
-//     return isFinite(number) ? number : 0
-// }
-
-const formatSet = (set, lift, routine) => Set({
+const formatSet = (set, lift) => Set({
     ...set,
-    // tmPercentage: (lift && routine) ? round(set.weight * 100 / (lift.rm*routine.trainingMax/100)) : 0
 })
 
 export const setsSelector = (exerciseId) => createSelector(
     [
         (state) => state.sets.entities,
-        liftExerciseSelector(exerciseId),
     ],
-    (sets, lift, routine) => sets
+    (sets) => sets
         .filter((set) => (set.exerciseId === exerciseId))
-        .map((set) => formatSet(set, lift))
+        .map((set) => formatSet(set))
 )
