3747659a0791af22b4764f562441c9c3cbae11f3
Julio Pavón <julio@jpavon.com>
2018-02-08 22:57:37 +0100

    change textarea lib

--- src/components/Form/Textarea/index.js
+++ src/components/Form/Textarea/index.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import classNames from 'classnames'
-import TextareaAutosize from 'react-textarea-autosize'
+import TextareaAutosize from 'react-autosize-textarea'
 
 import './style.css'
 
 const Textarea = ({name, className, ...rest}) => (
     <TextareaAutosize
-        minRows={2}
+        rows={2}
         name={name}
         className={classNames(
             'textarea',
