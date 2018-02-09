0183a873c21dc175f6cd3c8f2055643cd7428c39
Julio Pavón <julio@jpavon.com>
2018-02-09 13:34:27 +0100

    add img alt attr and html lang

--- src/components/Head/index.js
+++ src/components/Head/index.js
@@ -6,6 +6,8 @@ const Head = ({children}) => (
         defaultTitle="doroutine | Keep track of your workouts"
         titleTemplate="%s | doroutine"
     >
+        <html lang="en" />
+
         {/* Keep in sync with public/index.html */}
         <meta name="description" content="Workouts and routines creation web app for the gym. Keep track of sets, weight and reps for all your exercises. See your progress over time for your lifts." />
 
