041d4e21af2f4df0b4e55f52ef10f51a6f722da7
Julio Pavón <julio@jpavon.com>
2018-03-20 18:02:02 +0100

    changes logo / home header / footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -7,7 +7,9 @@ import './style.css'
 const Footer: React.SFC<{}> = () => (
     <footer className="footer">
         <div className="footer-inner">
-            <span className="footer-link">Support - <a href="mailto:hello@doroutine.com">hello@doroutine.com</a></span>
+            <span className="footer-item">
+                Support <a className="button footer-link" href="mailto:hello@doroutine.com">hello@doroutine.com</a>
+            </span>
             <br /><br />
             <img src={logoFull} alt="doroutine logo" /><br />
         </div>
