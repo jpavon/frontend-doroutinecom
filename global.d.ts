19ab30c46f4fd1c486bf0d168a66054bc2ee5007
Julio Pavón <julio@jpavon.com>
2018-06-19 23:04:52 +0200

    use latest react-scripts

--- global.d.ts
+++ global.d.ts
@@ -8,3 +8,8 @@ declare module 'store'
 declare module 'moveto'
 declare module 'react-animate-on-change'
 declare module 'react-spring'
+
+// declare module '*.scss' {
+//     const content: any
+//     export default content
+// }
