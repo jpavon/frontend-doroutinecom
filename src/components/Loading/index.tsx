8c85a70783e3b2ee21b8c74539354841ced1b600
Julio Pavón <julio@jpavon.com>
2018-07-01 18:07:46 +0200

    prevent showing loading for small amount of time

--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -2,8 +2,26 @@ import * as React from 'react'
 
 import './style.scss'
 
-const Loading: React.SFC<{}> = () => {
-    return <div className="spinner" />
+class Loading extends React.Component {
+    public timer: number
+
+    public readonly state = {
+        render: false
+    }
+
+    public componentDidMount() {
+        this.timer = window.setTimeout(() => {
+            this.setState({ render: true })
+        }, 300)
+    }
+
+    public componentWillUnmount() {
+        window.clearTimeout(this.timer)
+    }
+
+    public render() {
+        return this.state.render ? <div className="spinner" /> : null
+    }
 }
 
 export default Loading
