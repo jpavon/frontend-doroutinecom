30a18e9321882a04f0f61a898a8acac542244bbd
Julio Pavón <julio@jpavon.com>
2018-08-09 16:37:47 +0200

    only animate after mount

--- src/components/Transition/index.tsx
+++ src/components/Transition/index.tsx
@@ -1,9 +1,6 @@
 import * as React from 'react'
 import { Transition as SpringTransition } from 'react-spring'
 
-// tslint:disable-next-line:no-any
-type SFC<P = {}> = (props: P) => React.ReactElement<any> | null
-
 export interface Props {
     // tslint:disable-next-line:no-any
     children: React.ReactElement<any> | Array<React.ReactElement<any>>
@@ -11,36 +8,50 @@ export interface Props {
     animateHeight?: number
 }
 
-const Transition: SFC<Props> = (props) => {
-    if (Array.isArray(props.children) && props.children.length > 0) {
-        return (
-            <SpringTransition
-                keys={props.children.map((item) => item.key)}
-                from={{
-                    opacity: 0,
-                    height: props.animateHeight ? 0 : undefined
-                }}
-                enter={{
-                    opacity: 1,
-                    height: props.animateHeight
-                }}
-                leave={{
-                    opacity: 0,
-                    height: props.animateHeight ? 0 : undefined
-                }}
-            >
-                {props.children.map((item) => (styles: object) => (
-                    <div data-e2e={props.e2e} style={styles}>
-                        {item}
-                    </div>
-                ))}
-            </SpringTransition>
-        )
-    } else if (!Array.isArray(props.children)) {
-        return props.children
+class Transition extends React.Component<Props> {
+    private mounted: undefined | true
+
+    public componentDidMount() {
+        this.mounted = true
     }
 
-    return null
+    public render() {
+        const children = this.props.children
+
+        if (Array.isArray(children)) {
+            const childrenArr = children as Array<
+                React.ReactElement<{ key: string }>
+            >
+            return (
+                <SpringTransition
+                    items={childrenArr}
+                    keys={childrenArr.map((item) => item.key)}
+                    from={
+                        this.mounted && {
+                            opacity: 0,
+                            height: this.props.animateHeight ? 0 : undefined
+                        }
+                    }
+                    enter={{
+                        opacity: 1,
+                        height: this.props.animateHeight
+                    }}
+                    leave={{
+                        opacity: 0,
+                        height: this.props.animateHeight ? 0 : undefined
+                    }}
+                >
+                    {childrenArr.map((item) => (styles: object) => (
+                        <div data-e2e={this.props.e2e} style={styles}>
+                            {item}
+                        </div>
+                    ))}
+                </SpringTransition>
+            )
+        }
+
+        return children
+    }
 }
 
 export default Transition
